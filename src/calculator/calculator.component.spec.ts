dab6e2dea62ab6212aa227bc3e983530883267da
Shubham Roy <royshubham911@gmail.com>
2024-08-31 00:47:16 +0530

    substraction-test-cases

--- src/calculator/calculator.component.spec.ts
+++ src/calculator/calculator.component.spec.ts
@@ -34,4 +34,21 @@ describe('CalculatorComponent', () => {
   it('should be able to handle the line change between numbers passes in a string', ()=> {
     expect(component.calculateAdd('1\n3,5,\n8')).toBe(17)
   })
+  it('should return 0 for an empty string and null values', () => {
+    expect(component.calculateSubstract(null)).toBe(0);
+  });
+
+  it('should return 0 for an empty string', () => {
+    expect(component.calculateSubstract('')).toBe(0);
+  });
+
+  it('should return same value for a single number', () => {
+    expect(component.calculateSubstract(5)).toBe(5)
+  });
+  it('should be able to handle the elements in an array', ()=> {
+    expect(component.calculateSubstract([1, 2, 4])).toBe(7)
+  })
+  it('should be able to handle the line change between numbers passes in a string', ()=> {
+    expect(component.calculateSubstract('5,\n8')).toBe(17)
+  })
 });
